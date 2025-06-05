453cf9b9bdc2e894756a02e2230b9e2ef8c38df8
LF_JIN_VM <cnwjfx001@gmail.com>
2025-06-05 14:08:05 +1000

    lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,37 +7,49 @@
 
 using namespace std;
 
-void get_URL( const string& host, const string& path )
-{
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+void get_URL(const string &host, const string &path) {
+  Address addr = Address(host, "http");
+  TCPSocket sock = TCPSocket();
+  sock.connect(addr);
+  string message = "GET " + path + " HTTP/1.1\r\n" + "Host: "
+    + host + "\r\n" + "Connection: close\r\n\r\n";
+  
+  sock.write(message);
+  while (!sock.eof()) {
+    string response;
+    sock.read(response);
+    cout << response;
+  }
+
+  sock.close();
 }
 
-int main( int argc, char* argv[] )
-{
+
+int main(int argc, char *argv[]) {
   try {
-    if ( argc <= 0 ) {
+    if (argc <= 0) {
       abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
     }
 
-    auto args = span( argv, argc );
+    auto args = span(argv, argc);
 
     // The program takes two command-line arguments: the hostname and "path" part of the URL.
     // Print the usage message unless there are these two arguments (plus the program name
     // itself, so arg count = 3 in total).
-    if ( argc != 3 ) {
+    if (argc != 3) {
       cerr << "Usage: " << args.front() << " HOST PATH\n";
       cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
       return EXIT_FAILURE;
     }
 
     // Get the command-line arguments.
-    const string host { args[1] };
-    const string path { args[2] };
+    const string host{ args[1] };
+    const string path{ args[2] };
 
     // Call the student-written function.
-    get_URL( host, path );
-  } catch ( const exception& e ) {
+    get_URL(host, path);
+  }
+  catch (const exception &e) {
     cerr << e.what() << "\n";
     return EXIT_FAILURE;
   }
